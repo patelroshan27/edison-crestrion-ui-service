6563cd89c01b12a5052002700749c5c3ccecf921
Roshan Patel <rpnature.patel@gmail.com>
2024-02-03 09:59:24 -0500

    fixed styles for buttons

--- src/component/controls/Button.tsx
+++ src/component/controls/Button.tsx
@@ -67,7 +67,7 @@ const ButtonImpl: React.FC<ButtonImplProps> = ({
       className={classNames(
         'transition-all rounded-lg flex px-4 py-4 justify-between border border-primary',
         'focus:outline-none outline-none space-x-4 items-center',
-        isButtonActive ? 'bg-primary text-primary' : 'bg-black text-primary',
+        isButtonActive ? 'bg-primary text-primary' : 'bg-background text-primary',
         className,
       )}
       onClick={() => {
