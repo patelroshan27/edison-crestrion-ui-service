f658d8f17f74c49bcbecb892a24efd40abe1bce7
Roshan Patel <rpnature.patel@gmail.com>
2024-10-11 09:30:15 -0400

    feat: deploy script based on env variable

---
+++ src/config/first-floor/boys/SampSquare.tsx
@@ -0,0 +1,77 @@
+import { Lightbulb, LightbulbOff, Sun, SunDim } from 'lucide-react';
+import type { UIConfig } from 'config/Configs';
+import { commonRoomColorStates } from 'config/ConfigData';
+
+const SampSquare: UIConfig = {
+  rooms: [],
+  id: 123,
+  authProviderURL: '/crestron/passcodes/validate',
+  pharosApiPath: '/pharos/send',
+  zumApiPath: '/zum/send',
+  audioApiPath: '/audio/send',
+  mediaApiPath: '/mediaplayer/send',
+  projectorApiPath: '/video/projector/send',
+  authID: 'SampSquare',
+  pages: {
+    LIGHTS: {
+      name: 'Lights',
+      icon: Sun,
+      controls: {
+        lights: {
+          kind: 'group',
+          className:
+            'row-span-4 grid grid-cols-1 grid-rows-[1fr_1fr_1fr_1fr] gap-2',
+          controls: [
+            {
+              kind: 'light',
+              icon: Lightbulb,
+              title: 'Lights',
+              label: 'On',
+              apiCommands: [
+                { type: 'zum', payloads: [{ room: 'sampsquare', scene: '1' }] },
+              ],
+            },
+            {
+              kind: 'light',
+              icon: Sun,
+              title: 'Lights',
+              label: 'Medium',
+              apiCommands: [
+                { type: 'zum', payloads: [{ room: 'sampsquare', scene: '2' }] },
+              ],
+            },
+            {
+              kind: 'light',
+              icon: SunDim,
+              title: 'Lights',
+              label: 'Low',
+              apiCommands: [
+                { type: 'zum', payloads: [{ room: 'sampsquare', scene: '3' }] },
+              ],
+            },
+            {
+              kind: 'light',
+              icon: LightbulbOff,
+              title: 'Lights',
+              label: 'Off',
+              apiCommands: [
+                {
+                  type: 'zum',
+                  payloads: [{ room: 'sampsquare', scene: '16' }],
+                },
+              ],
+            },
+          ],
+        },
+        pharos: {
+          kind: 'pharos',
+          room: 'sampsquare',
+          className: 'row-span-4 col-span-3',
+          colorStates: commonRoomColorStates,
+        },
+      },
+    },
+  },
+};
+
+export default SampSquare;
