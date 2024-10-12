ae8e759666bcad10e0d4ed50a1499a84cbd2594d
Roshan Patel <rpnature.patel@gmail.com>
2024-10-12 11:58:49 -0400

    feat: added yogi hall config

--- src/config/first-floor/boys/Yogi.tsx
+++ src/config/first-floor/boys/Yogi.tsx
@@ -1,14 +1,18 @@
 import {
   ArrowDownToLine,
   ArrowUpToLine,
+  Bluetooth,
   Lightbulb,
   LightbulbOff,
+  Music2,
   PauseOctagon,
+  Speech,
   Sun,
   SunDim,
 } from 'lucide-react';
-import type { UIConfig } from 'config/Configs';
+import type { ApiCommand, UIConfig } from 'config/Configs';
 import { commonRoomColorStates } from 'config/ConfigData';
+import { MandirSvg } from 'svgs/Mandir';
 
 const Yogi: UIConfig = {
   rooms: [],
@@ -105,6 +109,212 @@ const Yogi: UIConfig = {
           room: 'yogi',
           className: 'row-span-4 col-span-3',
           colorStates: commonRoomColorStates,
+        },
+      },
+    },
+    AUDIO: {
+      name: 'Audio',
+      icon: Music2,
+      className: '!grid-cols-[1fr_1fr_1fr_2fr]',
+      controls: {
+        speaker: {
+          kind: 'audio',
+          icon: Music2,
+          label: 'Master',
+          playLabel: 'Unmute',
+          pauseLabel: 'Mute',
+          maxDB: 48311,
+          getVolCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'GS',
+              cmdName: 'Get Vol',
+              controlNumber: '16',
+              controlPosition: '',
+            },
+          },
+          volChangeCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'CS',
+              cmdName: 'Vol Change',
+              controlNumber: '16',
+              controlPosition: '',
+            },
+          },
+        },
+        mic1: {
+          kind: 'audio',
+          icon: Music2,
+          label: 'Mic1',
+          playLabel: 'Unmute',
+          pauseLabel: 'Mute',
+          maxDB: 56310,
+          getVolCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'GS',
+              cmdName: 'Get Vol',
+              controlNumber: '22',
+              controlPosition: '',
+            },
+          },
+          volChangeCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'CS',
+              cmdName: 'Vol Change',
+              controlNumber: '22',
+              controlPosition: '',
+            },
+          },
+        },
+        mic2: {
+          kind: 'audio',
+          icon: Music2,
+          label: 'Mic2',
+          playLabel: 'Unmute',
+          pauseLabel: 'Mute',
+          maxDB: 56310,
+          getVolCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'GS',
+              cmdName: 'Get Vol',
+              controlNumber: '23',
+              controlPosition: '',
+            },
+          },
+          volChangeCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'mandir',
+              cmdType: 'CS',
+              cmdName: 'Vol Change',
+              controlNumber: '23',
+              controlPosition: '',
+            },
+          },
+        },
+        sources: {
+          kind: 'group',
+          className:
+            'row-span-4 grid !grid-cols-2 !grid-rows-[1fr_1fr_1fr_1fr] gap-2',
+          getActiveValue: (
+            sendCommands: (commands: ApiCommand[]) => Promise<unknown[]>,
+          ) => {
+            return sendCommands([
+              {
+                type: 'audio',
+                payload: {
+                  dspId: 'mandir',
+                  cmdType: 'GS',
+                  cmdName: 'Source',
+                  controlNumber: '15',
+                  controlPosition: '',
+                },
+              },
+            ]).then((results) => (results[0] as number).toString());
+          },
+          parseActiveValueKey: (cmd: ApiCommand) =>
+            cmd.type === 'audio' ? cmd.payload.controlPosition : '',
+          controls: [
+            {
+              kind: 'toggle',
+              icon: Bluetooth,
+              title: 'Source',
+              label: 'Local',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'mandir',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '15',
+                    controlPosition: '0',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: MandirSvg,
+              title: 'Source',
+              label: 'Mandir',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'mandir',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '15',
+                    controlPosition: '16384',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              title: 'Source',
+              icon: Speech,
+              label: 'Sabha Hall',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'mandir',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '15',
+                    controlPosition: '32768',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: MandirSvg,
+              title: 'Source',
+              label: 'Wireless',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'mandir',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '15',
+                    controlPosition: '49151',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: Speech,
+              title: 'Source',
+              label: 'Spare',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'mandir',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '15',
+                    controlPosition: '65535',
+                  },
+                },
+              ],
+            },
+          ],
         },
       },
     },
