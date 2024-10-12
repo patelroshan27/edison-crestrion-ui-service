897b0978d839b20df880672e591ab50aa2a371b6
Roshan Patel <rpnature.patel@gmail.com>
2024-10-12 14:02:52 -0400

    feat: deploy script updated with relative paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,4 @@
-
-# Check if the environment is provided
-if [ -z "$1" ]; then
-  echo "Usage: ./deploy.sh [dev|prod]"
-  exit 1
-fi
-
-# Set directory paths based on the environment (dev or prod)
-if [ "$1" == "dev" ]; then
-  UI_BUILD_DIR=~/projects/crestron/edison/edison-crestron-ui-service/build/*
-  API_DIST_DIR=~/projects/crestron/edison/edison-auth-service/dist-ui/
-elif [ "$1" == "prod" ]; then
-  UI_BUILD_DIR=~/Desktop/crestron/edison-crestron-ui-service/build/*
-  API_DIST_DIR=~/Desktop/crestron/edison-auth-service/dist-ui/
-else
-  echo "Invalid environment. Use 'dev' or 'prod'."
-  exit 1
-fi
-
 yarn install
 yarn build
 
-cp -a $UI_BUILD_DIR $API_DIST_DIR
+cp -a ../build/* ../../edison-auth-service/dist-ui/
