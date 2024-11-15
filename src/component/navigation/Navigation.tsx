0a414262170194058841853188973e1293946f2d
Kaus Patel <kausdpatel@gmail.com>
2024-11-14 22:13:24 -0500

    make header responsive

--- src/component/navigation/Navigation.tsx
+++ src/component/navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import classNames from 'classnames';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {
   activeConfigState,
@@ -7,6 +6,7 @@ import {
   pageState,
 } from 'state/navigation';
 import { getAllowedPages } from 'utils/getAllowedPages';
+import { HeaderSelection } from './HeaderSelection';
 
 interface Props {
   className?: string;
@@ -19,33 +19,14 @@ const Navigation: React.FC<Props> = ({ className }: Props) => {
   const pages = getAllowedPages(activeConfig, loggedInUser);
 
   return (
-    <div
-      className={classNames(
-        'flex justify-between space-x-2 px-6 pt-2 pb-2 mb-2 w-full items-center border-b border-neutral-400',
-        className,
-      )}>
-      <div className="flex items-center space-x-2">
-        {pages.map((page) => {
-          const pageData = activeConfig.pages[page];
-          return (
-            <button
-              key={page}
-              type="button"
-              className={classNames(
-                'border border-neutral-400 bg-secondary px-3 py-3 flex items-center rounded-lg text-2xl',
-                activeTab === page
-                  ? '!bg-active text-primary-foreground'
-                  : 'bg-background text-primary',
-              )}
-              onClick={() => {
-                setActivePage(page);
-              }}>
-              {pageData.name}
-            </button>
-          );
-        })}
-      </div>
-    </div>
+    <HeaderSelection
+      className="mb-2 pb-2 border-b-1"
+      items={pages.map((p) => ({ title: activeConfig.pages[p].name, key: p }))}
+      selected={activeTab}
+      onSelect={setActivePage}
+      label="Tab"
+      placeholder="Select Tab"
+    />
   );
 };
 
