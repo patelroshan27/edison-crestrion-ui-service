d06b8f59e341a3bf9ae91a89d5bd315f85f7b0e1
Kaus Patel <kausdpatel@gmail.com>
2024-12-22 10:34:13 -0500

    add santoffice audio controls

--- src/config/first-floor/santnivas/SantOffice2.tsx
+++ src/config/first-floor/santnivas/SantOffice2.tsx
@@ -1,7 +1,18 @@
-import { Lightbulb, LightbulbOff, Sun, SunDim } from 'lucide-react';
-import { type UIConfig } from 'config/Configs';
+import {
+  Bluetooth,
+  Flower2,
+  Lightbulb,
+  LightbulbOff,
+  ListMusic,
+  Music2,
+  Speech,
+  Sun,
+  SunDim,
+} from 'lucide-react';
+import { type ApiCommand, type UIConfig } from 'config/Configs';
 import { commonRoomColorStates } from 'config/ConfigData';
 import {
+  audioResponsive,
   lightControlResponsive,
   pageResponsive,
   pharosResponsive,
@@ -117,6 +128,203 @@ const SantOffice2: UIConfig = {
           room: 'santoffice2',
           className: pharosResponsive,
           colorStates: commonRoomColorStates,
+        },
+      },
+    },
+    AUDIO: {
+      name: 'Audio',
+      icon: Music2,
+      className: `!grid-cols-[1fr_2fr_1fr] ${audioResponsive}`,
+      controls: {
+        speaker: {
+          kind: 'audio',
+          icon: Music2,
+          label: 'Master',
+          playLabel: 'Unmute',
+          pauseLabel: 'Mute',
+          getVolCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'bk',
+              cmdType: 'GS',
+              cmdName: 'Get Vol',
+              controlNumber: '23',
+              controlPosition: '',
+            },
+          },
+          volChangeCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'bk',
+              cmdType: 'CS',
+              cmdName: 'Vol Change',
+              controlNumber: '23',
+              controlPosition: '',
+            },
+          },
+          getMuteStatusCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'bk',
+              cmdType: 'GS',
+              cmdName: 'Mute Status',
+              controlNumber: '26',
+              controlPosition: '',
+            },
+          },
+          muteCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'bk',
+              cmdType: 'CS',
+              cmdName: 'Vol Mute',
+              controlNumber: '26',
+              controlPosition: '65535',
+            },
+          },
+          unMuteCmd: {
+            type: 'audio',
+            payload: {
+              dspId: 'bk',
+              cmdType: 'CS',
+              cmdName: 'Vol Unmute',
+              controlNumber: '26',
+              controlPosition: '0',
+            },
+          },
+        },
+        sources: {
+          kind: 'group',
+          className:
+            'row-span-4 grid !grid-cols-2 !grid-rows-[1fr_1fr_1fr_1fr] gap-2',
+          getActiveValue: (
+            sendCommands: (commands: ApiCommand[]) => Promise<unknown[]>,
+          ) => {
+            return sendCommands([
+              {
+                type: 'audio',
+                payload: {
+                  dspId: 'bk',
+                  cmdType: 'GS',
+                  cmdName: 'Source',
+                  controlNumber: '21',
+                  controlPosition: '',
+                },
+              },
+            ]).then((results) => (results[0] as number).toString());
+          },
+          parseActiveValueKey: (cmd: ApiCommand) =>
+            cmd.type === 'audio' ? cmd.payload.controlPosition : '',
+          controls: [
+            {
+              kind: 'toggle',
+              icon: Bluetooth,
+              title: 'Source',
+              label: 'Bluetooth',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '0',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: Speech,
+              title: 'Source',
+              label: 'Sabha Hall',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '13107',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              title: 'Source',
+              icon: Flower2,
+              label: 'Mandir',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '26214',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: Flower2,
+              title: 'Source',
+              label: 'Aksharpith',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '39321',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: ListMusic,
+              title: 'Source',
+              label: 'Media Player',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '52428',
+                  },
+                },
+              ],
+            },
+            {
+              kind: 'toggle',
+              icon: ListMusic,
+              title: 'Source',
+              label: 'Brahmanand',
+              apiCommands: [
+                {
+                  type: 'audio',
+                  payload: {
+                    dspId: 'bk',
+                    cmdType: 'CS',
+                    cmdName: 'Source',
+                    controlNumber: '21',
+                    controlPosition: '65535',
+                  },
+                },
+              ],
+            },
+          ],
         },
       },
     },
